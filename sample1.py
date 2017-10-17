e9eaca91ad586626fbee1d04811e70f243fd1998
vidya903 <vidya.903@gmail.com>
2017-10-16 21:43:39 -0700

    Print factorial of a number
    printing a python program to check factorial of a given number

--- sample1.py
+++ sample1.py
@@ -1,1 +1,15 @@
-print ("Hello Shashank Babu")
+# Factorial of a number
+def factorial(n):
+  rev=1
+  for i in range(1,n+1):
+    rev=rev*i
+  return rev
+
+
+while True:
+  n=int(input("Enter a Number "))
+  if (n==0):
+    break
+  else:
+    res = factorial(n)
+    print (res)
