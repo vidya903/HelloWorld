673e2eafbf58beb72dcd2dbd3346784a3f2486b6
vidya903 <vidya.903@gmail.com>
2017-10-18 16:04:18 -0700

    Printing a Largest Number
    printing a largest number from user given numbers

--- sample1.py
+++ sample1.py
@@ -1,15 +1,11 @@
-# Printing a Armstrong Number
-num = input("Enter a Number ")
-length = 1
-length = length + len(num)
-arm_stg = 0
-for char in (num):
-    arm_stg = arm_stg + (int(char) ** (length - 1))
-    num = int(num)
-    arm_stg = int(arm_stg)
-if (arm_stg - num == 0):
-    print("It is a Armstrong number")
-else:
-    print("It is not a Armstrong number")
+# Printing a Largest Number
+num=input("Enter Numbers with spaces ")
+numlist=num.split()
+largest=str(0)
+for i in numlist:
+   if largest<i:
+      largest=i
+print (largest)
+
 
 
