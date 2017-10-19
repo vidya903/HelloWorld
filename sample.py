30ade3082b1f30eee77232785000915a77324703
vidya903 <vidya.903@gmail.com>
2017-10-18 17:30:29 -0700

    Printing the number of vowels in a given input
    Finding the number of vowels that is (a,e,i,o,u) in a given user input

--- sample.py
+++ sample.py
@@ -1,10 +1,17 @@
-import os
-
-print("Hi how are you")
-
-# this change adds documentation to sample
-print(os.listdir("."))
-
-
+# Printing a LCM of a two numbers
+num1=int(input("Enter Numbers one: "))
+num2=int(input("Enter Numbers two: "))
+value=1
+for i in range(2,num1):
+  if num1%i==0:
+     if i==2 or i==3:
+        value=(value*i)
+     else i//2==2:
+         value=(value)*i/2
+     else i//3==:
+         value=(value)*i/3
+     elif:
+         value=value*i
+      print (value)
 
 
