1fea2dd874399746523a723cecbb1883f6d7ee01
vidya903 <vidya.903@gmail.com>
2017-10-18 17:25:26 -0700

    Printing Number od Vowels
    Finding number of vowels thats is (a,e,i,o,u) in user entered input

--- sample1.py
+++ sample1.py
@@ -1,11 +1,18 @@
-# Printing a Largest Number
-num=input("Enter Numbers with spaces ")
-numlist=num.split()
-largest=str(0)
-for i in numlist:
-   if largest<i:
-      largest=i
-print (largest)
+# Counting Number of vowels
+name=input("Enter a Name")
+count=0
+for char in name:
+  if char=="a":
+    count=count+1
+  elif char =="e":
+    count=count+1
+  elif char =="i":
+    count=count+1
+  elif char=="o":
+    count=count+1
+  elif char =="u":
+    count=count+1
+print("Total Number of vowels are ",count)
 
 
 
